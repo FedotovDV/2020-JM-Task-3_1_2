422a09c22a50e7bbfc32fec095f53e641e211d14
Dmitriy <fed3375546@yandex.ru>
2020-08-24 21:33:48 +0300

    jQuery  NG

--- src/main/java/ru/javamentor/task_3_1_2/controllers/UserController.java
+++ src/main/java/ru/javamentor/task_3_1_2/controllers/UserController.java
@@ -13,6 +13,7 @@ import ru.javamentor.task_3_1_2.service.UserService;
 import ru.javamentor.task_3_1_2.util.UserValidator;
 
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -40,7 +41,7 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView();
         List<User> users = userService.findAll();
         modelAndView.addObject("message", "MESSAGE");
-        modelAndView.setViewName("test");
+        modelAndView.setViewName("/test");
         return modelAndView;
     }
 
@@ -49,7 +50,7 @@ public class UserController {
     public ModelAndView indexPost(@ModelAttribute("usernew") String user) {
         System.out.println(user);
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("/index");
         return modelAndView;
     }
 
@@ -58,7 +59,7 @@ public class UserController {
     public String registration(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("roles", Role.values());
-        return "registration";
+        return "/registration";
     }
 
     @PostMapping("/registration")
@@ -68,15 +69,25 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView();
         userValidator.validate(user, result);
         if (result.hasErrors()) {
-            modelAndView.setViewName("registration");
+            modelAndView.setViewName("/registration");
             return modelAndView;
         }
         setUserRoles(user, userRole, adminRole);
         userService.saveUser(user);
-        modelAndView.setViewName("user");
+        modelAndView.setViewName("/user");
         return modelAndView;
     }
 
+    private void setUserRoles(@ModelAttribute("admin/user") User user, @RequestParam(value = "userRole", required = false) String userRole, @RequestParam(value = "adminRole", required = false) String adminRole) {
+        Set<Role> roles = new HashSet<>();
+        if (userRole != null) {
+            roles.add(Role.USER);
+        }
+        if (adminRole != null) {
+            roles.add(Role.ADMIN);
+        }
+        user.setRoles(roles);
+    }
 
     @GetMapping(value = "/user")
     public ModelAndView userForm(Authentication authentication) {
@@ -92,126 +103,49 @@ public class UserController {
             }
         }
 
-        modelAndView.addObject("titleRole", titleRole);
+//        modelAndView.addObject("titleRole", titleRole);
         modelAndView.addObject("user", user);
         modelAndView.addObject("authentication", authentication);
-        modelAndView.setViewName("user");
+        modelAndView.setViewName("/user");
         return modelAndView;
     }
 
 
     @GetMapping("/admin")
     public ModelAndView admin(ModelAndView modelAndView, Authentication authentication) {
-        String email = authentication.getName();
-        User user = (User) userService.loadUserByUsername(email);
         String titleRole = "ADMIN";
         List<User> users = userService.findAll();
-
+        System.out.println("ModelAndView admin 1 "+modelAndView.toString());
+        String email = authentication.getName();
+        User user = (User) userService.loadUserByUsername(email);
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("roles", new HashSet<Role>());
         modelAndView.addObject("titleRole", titleRole);
         modelAndView.addObject("users", users);
         modelAndView.addObject("authentication", authentication);
         modelAndView.addObject("usernew", new User());
         modelAndView.addObject("rolesnew", new HashSet<Role>());
-        modelAndView.setViewName("admin-page");
+        modelAndView.setViewName("/admin-page");
+        System.out.println("ModelAndView admin 2 "+ modelAndView.toString());
         return modelAndView;
     }
 
-    @PostMapping({"/admin"})
-    public ModelAndView adminPost(@ModelAttribute("usernew") User user, BindingResult result,
-                                  @RequestParam(value = "userRole", required = false) String userRole,
-                                  @RequestParam(value = "adminRole", required = false) String adminRole) {
-        System.out.println("User " + user.getName());
-        userValidator.validate(user, result);
-        if (result.hasErrors()) {
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("add-new-user");
-            return modelAndView;
-        }
-        setUserRoles(user, userRole, adminRole);
-        userService.saveUser(user);
-        return new ModelAndView("redirect:/admin");
-    }
-
-    @GetMapping("/admin/add")
-    public ModelAndView addGet() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("add-new-user");
-        model.addObject("user", new User());
-        model.addObject("roles", new HashSet<Role>());
-        return model;
-    }
 
 
-    @PostMapping({"/admin/add"})
-    public ModelAndView addPost(@ModelAttribute("usernew") User user,
-                                @RequestParam(value = "userRole", required = false) String userRole,
-                                @RequestParam(value = "adminRole", required = false) String adminRole) {
+    @GetMapping("/admin/update")
+    @ResponseBody
+    public   User updateUser(@RequestParam("id") Long id){
 
-//        userValidator.validate(user, result);
-//        if (result.hasErrors()) {
-//            ModelAndView modelAndView = new ModelAndView();
-//            modelAndView.setViewName("add-new-user");
-//            return modelAndView;
-//        }
-        setUserRoles(user, userRole, adminRole);
-        userService.saveUser(user);
-        return new ModelAndView("redirect:/admin");
-    }
+        System.out.println("updateUser "+ id);
 
-
-//    @GetMapping("/admin/update")
-//
-//    public User updateGet(@RequestParam Long id) {
-//        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!User updateGet = " + id);
-//        User user = userService.findById(id);
-//        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!User  " + user.getName());
-//        return user;
-//    }
-
-    @GetMapping("/admin/update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model){
         User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "admin";
-    }
 
-    @PostMapping("/admin/update")
-    public ModelAndView updatePost(@ModelAttribute("admin/user") User user,
-                                   @RequestParam(value = "userRole", required = false) String userRole,
-                                   @RequestParam(value = "adminRole", required = false) String adminRole) {
-
-        setUserRoles(user, userRole, adminRole);
-        userService.saveUser(user);
-        return new ModelAndView("redirect:/admin");
-    }
-
-    private void setUserRoles(@ModelAttribute("admin/user") User user, @RequestParam(value = "userRole", required = false) String userRole, @RequestParam(value = "adminRole", required = false) String adminRole) {
-        Set<Role> roles = new HashSet<>();
-        if (userRole != null) {
-            roles.add(Role.USER);
-        }
-        if (adminRole != null) {
-            roles.add(Role.ADMIN);
-        }
-        user.setRoles(roles);
+        return user;
     }
 
 
-    @GetMapping("/admin/delete{id}")
-    public ModelAndView deleteGet(@RequestParam("id") Long id, ModelAndView model) {
-        model.setViewName("delete-user");
-        User user = userService.findById(id);
-        model.addObject("user", user);
-        model.addObject("roles", new HashSet<Role>());
-        return model;
-    }
 
 
-    @PostMapping("/admin/delete{id}")
-    public ModelAndView deletePost(@RequestParam("id") Long id, ModelAndView model) {
-        userService.deleteById(id);
-        return new ModelAndView("redirect:/admin");
-    }
 
     @GetMapping(value = "/login")
     public String loginForm() {
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,12 +1,13 @@
 $('document').ready(function () {
     $('button-edit').on('click', function (event) {
         event.preventDefault();
-        alert("edit");
-        let href = $(this).attr('href');
-        alert(href);
+        const href = $(this).attr('href');
+
         $.get(href, function (user) {
-            $('#id').val(user.id);
+            alert(user.name);
+            $('body').add('<div th:insert="blocks/edit-user::edit-form"></div>');
+            $('#editModal').modal();
         });
-        $('#editModal').modal();
+
     });
 })
