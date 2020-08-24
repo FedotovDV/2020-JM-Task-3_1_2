116602982391f53cc8626fa928a741dcd6ffb671
Dmitriy <fed3375546@yandex.ru>
2020-08-24 23:29:28 +0300

    jQuery OK

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -4,10 +4,13 @@ $('document').ready(function () {
         const href = $(this).attr('href');
 
         $.get(href, function (user) {
-            alert(user.name);
-            // $('body').add('<div th:insert="blocks/edit-user::edit-form"></div>');
-            // $('#editModal').modal();
+            $('#id').attr('value', user.id);
+            $('#name').attr('value', user.name);
+            $('#surname').attr('value', user.surname);
+            $('#age').attr('value', user.age);
+            $('#email').attr('value', user.email);
+            $('#password').attr('value', user.password);
         });
-
+        $('#editModal').showModal();
     });
 })
